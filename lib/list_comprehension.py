c26a39a77617ddcb1208ecb36465e36f94a9c258
Hass <hassmroueh@gmail.com>
2024-11-12 22:47:45 -0500

    List comp lab complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    num_list = [n for n in num_list if n% 2 ==0 ] 
+    return num_list
 
 def make_exclamation(sentence_list):
-    pass
+    return[str +"!"for str in sentence_list]
